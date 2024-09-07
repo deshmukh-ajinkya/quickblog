6b2873b8d6d152026282bf1c9d34c68a9e2e3671
deshmukh-ajinkya <deshmukhasb@gmail.com>
2024-09-07 12:23:00 +0530

    test: test layout

--- src/app/(private)/insight/page.tsx
+++ src/app/(private)/insight/page.tsx
@@ -1,6 +1,107 @@
+'use client';
 import { Box, Typography } from '@mui/material';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement
+} from 'chart.js';
 import React from 'react';
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import './style.css';
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement
+);
+
+const data = {
+  datasets: [
+    {
+      label: 'Likes by Category',
+      data: [300, 50, 100],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+    }
+  ]
+};
+
+const likeData = {
+  labels: ['Blog 1', 'Blog 2', 'Blog 3'],
+  datasets: [
+    {
+      label: 'Likes',
+      data: [65, 59, 80],
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderWidth: 0.5,
+      barThickness: 50
+    }
+  ]
+};
+
+const countData = {
+  labels: ['Blog 1', 'Blog 2', 'Blog 3'],
+  datasets: [
+    {
+      label: 'Count',
+      data: [65, 59, 80],
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderWidth: 2,
+      tension: 0.1 // Add this line for a smooth line chart
+    }
+  ]
+};
+
+const doughnutOptions = {
+  responsive: true,
+  maintainAspectRatio: false
+};
+
+const barOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      ticks: {
+        autoSkip: false
+      },
+      grid: {
+        display: false
+      }
+    },
+    y: {
+      beginAtZero: true
+    }
+  }
+};
+
+const lineOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      grid: {
+        display: false
+      }
+    },
+    y: {
+      beginAtZero: true
+    }
+  }
+};
 
 function Analytic(): React.ReactElement {
   return (
@@ -10,25 +111,33 @@ function Analytic(): React.ReactElement {
       </Typography>
       <Box sx={{ gridRow: 2, gridColumn: '1/2' }}>
         <Typography className="section-heading">Date</Typography>
-        <Typography>Jan-2024</Typography>
-        <Typography>Dec-2024</Typography>
+        <Box sx={{ display: 'flex' }}>
+          <Typography>Jan-2024 - </Typography>
+          <Typography>Dec-2024</Typography>
+        </Box>
       </Box>
       <Box sx={{ gridRow: 3 }}>
         <Typography className="section-heading">Liked By Category</Typography>
-        <Box>Like By Category Pie Chart</Box>
+        <Box className="doughnut-chart-container" sx={{ display: 'flex', flexGrow: '1' }}>
+          <Doughnut data={data} options={doughnutOptions} />
+        </Box>
       </Box>
-      <Box sx={{ gridRow: 4, overflow: 'scroll', scrollbarWidth: 'none' }}>
+      <Box className="scrollable-section">
         <Typography className="section-heading">Post Overview</Typography>
         <Box>Post Overview Table</Box>
       </Box>
       <Box className="charts-container">
-        <Box>
+        <Box sx={{ display: 'flex', flexGrow: '1' }}>
           <Typography className="section-heading">Most Liked Blog</Typography>
-          <Box>Most Liked Blog Graph</Box>
+          <Box className="doughnut-chart-container" sx={{ display: 'flex', flexGrow: '1' }}>
+            <Bar data={likeData} options={barOptions} />
+          </Box>
         </Box>
-        <Box>
+        <Box sx={{ display: 'flex', flexGrow: '1' }}>
           <Typography className="section-heading">Visitors Count</Typography>
-          <Box>Visitors Count Graph</Box>
+          <Box className="doughnut-chart-container" sx={{ display: 'flex', flexGrow: '1' }}>
+            <Line data={countData} options={lineOptions} />
+          </Box>
         </Box>
       </Box>
     </Box>
