105e1aacdc03c19777df8bed04deb6b1cfdcd70d
deshmukh-ajinkya <deshmukhasb@gmail.com>
2024-09-07 16:34:49 +0530

    feat: auth screen layout created

--- src/app/(protected)/register/page.tsx
+++ src/app/(protected)/register/page.tsx
@@ -1,7 +1,115 @@
-import React from 'react';
+'use client';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { Box, Button, TextField, Typography, IconButton, InputAdornment } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import Logo from '../../../../public/icon.png';
+
+function PasswordField({
+  label,
+  showPassword,
+  handleClickShowPassword
+}: {
+  label: string;
+  showPassword: boolean;
+  handleClickShowPassword: () => void;
+}): React.ReactElement {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+      <TextField
+        size="small"
+        type={showPassword ? 'text' : 'password'}
+        placeholder={label}
+        fullWidth
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={handleClickShowPassword} edge="end">
+                {showPassword ? (
+                  <VisibilityOffIcon fontSize="small" />
+                ) : (
+                  <RemoveRedEyeIcon fontSize="small" />
+                )}
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
+      />
+      <Typography sx={{ color: 'red', fontSize: '0.75rem', visibility: 'hidden' }}>
+        Validation message
+      </Typography>
+    </Box>
+  );
+}
 
 function Register(): React.ReactElement {
-  return <div>Register</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateRows: 'auto 36px auto 36px auto 36px auto',
+        gap: '1rem',
+        placeItems: 'center',
+        margin: 'auto',
+        minWidth: '250px',
+        height: 'fit-content',
+        padding: '1rem',
+        borderRadius: '0.5rem',
+        border: '1px solid #008AE6',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}>
+      <Image
+        src={Logo}
+        alt="logo"
+        style={{ maxWidth: '8rem', height: 'auto', paddingBottom: '0.5rem' }}
+      />
+
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <TextField size="small" placeholder="Full Name" fullWidth />
+        <Typography sx={{ color: 'red', fontSize: '0.75rem', visibility: 'hidden' }}>
+          Validation message
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <TextField size="small" placeholder="Email" fullWidth />
+        <Typography sx={{ color: 'red', fontSize: '0.75rem', visibility: 'hidden' }}>
+          Validation Message
+        </Typography>
+      </Box>
+
+      <PasswordField
+        label="Password"
+        showPassword={showPassword}
+        handleClickShowPassword={() => setShowPassword((prev) => !prev)}
+      />
+      <PasswordField
+        label="Confirm Password"
+        showPassword={showConfirmPassword}
+        handleClickShowPassword={() => setShowConfirmPassword((prev) => !prev)}
+      />
+
+      <Button
+        variant="contained"
+        color="primary"
+        size="medium"
+        sx={{ textTransform: 'capitalize', width: '100%', fontSize: '0.7rem' }}>
+        Register
+      </Button>
+
+      <Link href={'/login'} style={{ fontSize: '0.8rem', color: '#008AE6' }}>
+        Already have an account? Login
+      </Link>
+    </Box>
+  );
 }
 
 export default Register;
--- src/app/(protected)/reset/page.tsx
+++ src/app/(protected)/reset/page.tsx
@@ -1,7 +1,126 @@
-import React from 'react';
+'use client';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import Logo from '../../../../public/icon.png';
+
+const steps = ['Enter Email', 'Enter OTP', 'Set New Password'];
 
 function Reset(): React.ReactElement {
-  return <div>Reset</div>;
+  const [activeStep, setActiveStep] = useState(0);
+  const [email, setEmail] = useState('');
+  const [otp, setOtp] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleStepChange = (direction: 'next' | 'back'): void => {
+    setActiveStep((prevStep) => prevStep + (direction === 'next' ? 1 : -1));
+  };
+
+  const handleReset = (): void => {
+    setActiveStep(0);
+    setEmail('');
+    setOtp('');
+    setPassword('');
+  };
+
+  const handleSubmit = (): void => {
+    if (activeStep === steps.length - 1) {
+      handleReset();
+    } else {
+      handleStepChange('next');
+    }
+  };
+
+  const renderStepContent = (): React.ReactElement | null => {
+    if (activeStep === 0) {
+      return (
+        <TextField
+          size="small"
+          placeholder="Email"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      );
+    } else if (activeStep === 1) {
+      return (
+        <TextField
+          size="small"
+          placeholder="Enter OTP"
+          fullWidth
+          value={otp}
+          onChange={(e) => setOtp(e.target.value)}
+        />
+      );
+    } else if (activeStep === 2) {
+      return (
+        <TextField
+          size="small"
+          placeholder="Enter New Password"
+          type="password"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateRows: 'auto 36px auto 36px fit-content',
+        gap: '0.5rem',
+        placeItems: 'center',
+        margin: 'auto',
+        minWidth: '250px',
+        height: 'fit-content',
+        padding: '1rem',
+        borderRadius: '0.5rem',
+        border: '1px solid #008AE6',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}>
+      <Image src={Logo} alt="logo" style={{ maxWidth: '8rem', height: 'auto' }} />
+
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        {renderStepContent()}
+        <Typography sx={{ color: 'red', fontSize: '0.75rem', visibility: 'hidden' }}>
+          Validation message
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+        {activeStep > 0 && (
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ textTransform: 'capitalize', fontSize: '0.7rem', justifySelf: 'center' }}
+            onClick={() => handleStepChange('back')}>
+            Back
+          </Button>
+        )}
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ textTransform: 'capitalize', fontSize: '0.7rem', justifySelf: 'center' }}
+          onClick={handleSubmit}>
+          {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
+        </Button>
+      </Box>
+
+      <Link href="/login" style={{ fontSize: '0.8rem', color: '#008AE6' }}>
+        Back to login
+      </Link>
+    </Box>
+  );
 }
 
 export default Reset;
